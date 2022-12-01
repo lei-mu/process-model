55dc2a3fa401ef1036e96d580d1b9a983de88af3
luch <webwork.s@qq.com>
2022-12-01 18:00:35 +0800

    build: 打包方式换成rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,70 @@
-import {version} from './package.json'
-import resolve from 'rollup-plugin-node-resolve'
-import babel from '@rollup/plugin-babel'
+// import {version} from './package.json'
+import resolve from '@rollup/plugin-node-resolve';
+import {babel} from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs'
 // https://www.npmjs.com/package/rollup-plugin-terser
 import {terser} from "rollup-plugin-terser"
 import json from '@rollup/plugin-json'
 
+import fs from 'fs'
+
+const packageJsonData = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+
 const libName = 'ProcessModel'
+const version = packageJsonData.version
 export default {
+  makeAbsoluteExternalsRelative: true,
+  preserveEntrySignatures: 'strict',
   input: 'src/index.js',
   // sourcemap: false,
   output: [
     {
       name: libName,
       banner: `/*! ${libName} version ${version} */`,
-      file: 'dist/processModel.js',
-      format: 'umd'
+      file: 'dist/process-model.umd.js',
+      format: 'umd',
+      esModule: true,
+      generatedCode: {
+        reservedNamesAsProps: false
+      },
+      interop: 'compat',
+      systemNullSetters: false
     },
     {
       name: libName,
       banner: `/*! ${libName} version ${version} */`,
-      file: 'dist/processModel.browser.js',
-      format: 'iife'
+      file: 'dist/process-model.browser.js',
+      format: 'iife',
+      esModule: true,
+      generatedCode: {
+        reservedNamesAsProps: false
+      },
+      interop: 'compat',
+      systemNullSetters: false
     },
     {
       name: libName,
       banner: `/*! ${libName} version ${version} */`,
       file: 'dist/process-model.common.js',
-      format: 'cjs'
+      format: 'cjs',
+      esModule: true,
+      generatedCode: {
+        reservedNamesAsProps: false
+      },
+      interop: 'compat',
+      systemNullSetters: false
     },
     {
       name: libName,
       banner: `/*! ${libName} version ${version} */`,
       file: 'dist/process-model.esm.mjs',
-      format: 'esm'
+      format: 'esm',
+      esModule: true,
+      generatedCode: {
+        reservedNamesAsProps: false
+      },
+      interop: 'compat',
+      systemNullSetters: false
     }
   ],
   // customConfig (config) {
@@ -44,12 +75,14 @@ export default {
   plugins: [
     json(),
     resolve({
-      preferBuiltins: true
+      moduleDirectories: ['node_modules']
+      // preferBuiltins: true
     }),
     commonjs(),
     babel({
-      babelHelpers: 'runtime',
-      exclude: '**/node_modules/**'
+      babelHelpers: 'bundled'
+      // babelHelpers: 'runtime',
+      // exclude: '**/node_modules/**'
       // allowAllFormats: true,
       // presets: ['@babel/preset-env'],
       // plugins: [['@babel/plugin-transform-runtime', {useESModules: false}]]
@@ -78,7 +111,6 @@ export default {
 
   ],
   // globals: {
-  //   'CryptoJS': true
   // },
   external: []
 }
